e95f1005530d676b39b0c6213cc8a0cf8ec374b0
rmHawk765 <171146113+rmHawk765@users.noreply.github.com>
2025-07-20 17:34:23 +0100

    IT (somewhat) WORKS!

--- src/structs/client/IgniteClient.ts
+++ src/structs/client/IgniteClient.ts
@@ -12,6 +12,8 @@ import {
 import { EventListenerComponent } from "../components/eventListeners/index.js";
 import type { ComponentType } from "../components/ComponentType.js";
 import winston from "winston";
+import { dir } from "node:console";
+import { pathToFileURL } from "node:url";
 
 /**
  * The base Ignite client class.
@@ -37,15 +39,16 @@ export class IgniteClient {
     this.logger = winston.createLogger({
       ...options,
       format: loggerFormat,
+      transports: [new winston.transports.Console()],
     });
 
-    this.basePluginsDir = options.basePluginsDir;
+    this.basePluginsDir = path.resolve(options.basePluginsDir);
     this.components = [];
   }
 
   async login(token: string) {
     // Load all components
-    const modulePaths: string[] = [];
+    const moduleUrls: string[] = [];
 
     {
       const dirEntries = fs.readdirSync(this.basePluginsDir, {
@@ -54,191 +57,204 @@ export class IgniteClient {
       });
 
       for (const dirEntry of dirEntries) {
-        const entryPath = path.relative(
-          this.basePluginsDir,
-          path.join(dirEntry.parentPath, dirEntry.name)
+        const entryAbsolutePath = path.resolve(
+          dirEntry.parentPath,
+          dirEntry.name
         );
 
         if (
           dirEntry.isFile() &&
-          (path.extname(dirEntry.name) === ".js" ||
-            path.extname(dirEntry.name) === ".mjs" ||
-            path.extname(dirEntry.name) === ".ts" ||
-            path.extname(dirEntry.name) === ".mts")
+          [".js", ".ts", ".mjs", ".mts"].includes(path.extname(dirEntry.name))
         ) {
-          modulePaths.push(entryPath);
+          const moduleUrl = pathToFileURL(entryAbsolutePath).href;
+          moduleUrls.push(moduleUrl);
         }
       }
-    }
 
-    for (const modulePath of modulePaths) {
-      const module = await import(modulePath);
+      for (const modulePath of moduleUrls) {
+        const logger = this.logger;
+        async function safeImportModule(): Promise<any> {
+          try {
+            return await import(modulePath);
+          } catch (error) {
+            logger.error(
+              `Failed to import module at:\n${modulePath}\nError: ${error}`
+            );
+          }
+        }
+        const module: any = await safeImportModule();
+        if (!module) continue;
 
-      for (const exported of module) {
-        if (
-          !(
-            exported instanceof SlashCommandComponent ||
-            exported instanceof ContextMenuCommandComponent ||
-            exported instanceof EventListenerComponent
+        const allExports = Object.values(module);
+
+        for (const exported of allExports) {
+          if (
+            !(
+              exported instanceof SlashCommandComponent ||
+              exported instanceof ContextMenuCommandComponent ||
+              exported instanceof EventListenerComponent
+            )
           )
-        )
-          continue;
+            continue;
 
-        this.components.push(exported);
+          this.components.push(exported);
 
-        this.logger.debug(`Registered component:\n${exported}`);
-      }
-    }
-
-    this.logger.info("Registered all components");
-
-    // Register non-interactionCreate event listeners
-    for (const component of this.components) {
-      if (!(component instanceof EventListenerComponent)) continue;
-      if (component.event === Events.InteractionCreate) continue;
-
-      this.discordClient.on(component.event, component.run);
-    }
-
-    // Run interactionCreate listeners and handle commands
-    this.discordClient.on(Events.InteractionCreate, (interaction) => {
-      for (const component of this.components) {
-        if (
-          !interaction.isCommand() &&
-          component instanceof EventListenerComponent &&
-          component.event === Events.InteractionCreate
-        ) {
-          component.run(interaction);
-
-          return;
+          this.logger.debug(`Registered component:\n${exported}`);
         }
+      }
 
-        if (
-          interaction.isCommand() &&
-          (component instanceof SlashCommandComponent ||
-            component instanceof ContextMenuCommandComponent) &&
-          interaction.commandName !== component.builder.name
-        )
-          continue;
+      this.logger.info("Registered all components");
 
-        if (
-          interaction.isChatInputCommand() &&
-          !(component instanceof SlashCommandComponent)
-        )
-          continue;
+      // Register non-interactionCreate event listeners
+      for (const component of this.components) {
+        if (!(component instanceof EventListenerComponent)) continue;
+        if (component.event === Events.InteractionCreate) continue;
 
-        if (
-          interaction.isContextMenuCommand() &&
-          !(component instanceof ContextMenuCommandComponent)
-        )
-          continue;
+        this.discordClient.on(component.event, component.run);
+      }
 
-        if (
-          interaction.isChatInputCommand() &&
-          component instanceof SlashCommandComponent
-        ) {
-          const subcommandGroupName =
-            interaction.options.getSubcommandGroup(false);
-          const subcommandName = interaction.options.getSubcommand(false);
-
-          if (!subcommandName) {
+      // Run interactionCreate listeners and handle commands
+      this.discordClient.on(Events.InteractionCreate, (interaction) => {
+        for (const component of this.components) {
+          if (
+            !interaction.isCommand() &&
+            component instanceof EventListenerComponent &&
+            component.event === Events.InteractionCreate
+          ) {
             component.run(interaction);
 
             return;
           }
 
-          for (const subcomponent of component.subcomponents) {
-            if (
-              !(
-                subcomponent instanceof SlashCommandSubcommandComponent ||
-                subcomponent instanceof SlashCommandSubcommandGroupComponent
-              )
-            )
-              continue;
+          if (
+            interaction.isCommand() &&
+            (component instanceof SlashCommandComponent ||
+              component instanceof ContextMenuCommandComponent) &&
+            interaction.commandName !== component.builder.name
+          )
+            continue;
 
-            if (subcomponent instanceof SlashCommandSubcommandGroupComponent) {
-              if (subcomponent.builder.name !== subcommandGroupName) continue;
+          if (
+            interaction.isChatInputCommand() &&
+            !(component instanceof SlashCommandComponent)
+          )
+            continue;
 
-              for (const subcommand of subcomponent.subcomponents) {
-                if (subcommand.builder.name !== subcommandName) continue;
+          if (
+            interaction.isContextMenuCommand() &&
+            !(component instanceof ContextMenuCommandComponent)
+          )
+            continue;
 
-                subcommand.run(interaction);
+          if (
+            interaction.isChatInputCommand() &&
+            component instanceof SlashCommandComponent
+          ) {
+            const subcommandGroupName =
+              interaction.options.getSubcommandGroup(false);
+            const subcommandName = interaction.options.getSubcommand(false);
 
-                return;
-              }
-            } else {
-              if (subcomponent.builder.name !== subcommandName) continue;
-
-              subcomponent.run(interaction);
+            if (!subcommandName) {
+              component.run(interaction);
 
               return;
             }
+
+            for (const subcomponent of component.subcomponents) {
+              if (
+                !(
+                  subcomponent instanceof SlashCommandSubcommandComponent ||
+                  subcomponent instanceof SlashCommandSubcommandGroupComponent
+                )
+              )
+                continue;
+
+              if (
+                subcomponent instanceof SlashCommandSubcommandGroupComponent
+              ) {
+                if (subcomponent.builder.name !== subcommandGroupName) continue;
+
+                for (const subcommand of subcomponent.subcomponents) {
+                  if (subcommand.builder.name !== subcommandName) continue;
+
+                  subcommand.run(interaction);
+
+                  return;
+                }
+              } else {
+                if (subcomponent.builder.name !== subcommandName) continue;
+
+                subcomponent.run(interaction);
+
+                return;
+              }
+            }
+          }
+
+          if (interaction.isContextMenuCommand()) {
+            if (!(component instanceof ContextMenuCommandComponent)) continue;
+
+            component.run(interaction);
+
+            return;
           }
         }
+      });
 
-        if (interaction.isContextMenuCommand()) {
-          if (!(component instanceof ContextMenuCommandComponent)) continue;
+      // Prep for command registration
+      await this.discordClient.login(token);
+      await new Promise((resolve) => {
+        this.discordClient.once(Events.ClientReady, resolve);
+      });
 
-          component.run(interaction);
+      if (!this.discordClient.isReady()) return;
 
-          return;
-        }
-      }
-    });
+      // Register application commands
+      const commands = await this.discordClient.application.commands.fetch();
 
-    // Prep for command registration
-    await this.discordClient.login(token);
-    await new Promise((resolve) => {
-      this.discordClient.once(Events.ClientReady, resolve);
-    });
+      const commandManager = this.discordClient.application.commands;
 
-    if (!this.discordClient.isReady()) return;
+      for (const component of this.components) {
+        if (!(component instanceof ApplicationCommandBaseComponent)) continue;
 
-    // Register application commands
-    const commands = await this.discordClient.application.commands.fetch();
+        const fullCommandData = (() => {
+          if (component instanceof ContextMenuCommandComponent) {
+            return component.builder;
+          }
 
-    const commandManager = this.discordClient.application.commands;
+          for (const subcomponent of component.subcomponents) {
+            if (subcomponent instanceof SlashCommandSubcommandComponent) {
+              component.builder.addSubcommand(subcomponent.builder);
+            } else {
+              component.builder.addSubcommandGroup(subcomponent.builder);
+            }
+          }
 
-    for (const component of this.components) {
-      if (!(component instanceof ApplicationCommandBaseComponent)) continue;
-
-      const fullCommandData = (() => {
-        if (component instanceof ContextMenuCommandComponent) {
           return component.builder;
-        }
+        })();
 
-        for (const subcomponent of component.subcomponents) {
-          if (subcomponent instanceof SlashCommandSubcommandComponent) {
-            component.builder.addSubcommand(subcomponent.builder);
-          } else {
-            component.builder.addSubcommandGroup(subcomponent.builder);
+        if (component.options?.commandIds) {
+          for (const id of component.options.commandIds) {
+            const command = commands.get(id);
+
+            if (
+              !command?.equals(
+                // The cast is necessary here as otherwise you get a stupid error
+                // about a single missing property which is only available
+                // for the 'education guilds'
+                // TODO: though I do want a proper fix for this
+                component.builder.toJSON() as ApplicationCommandData
+              )
+            ) {
+              await commands.get(id)?.edit(fullCommandData);
+            }
           }
+        } else {
+          await commandManager.create(fullCommandData);
         }
-
-        return component.builder;
-      })();
-
-      if (component.options?.commandIds) {
-        for (const id of component.options.commandIds) {
-          const command = commands.get(id);
-
-          if (
-            !command?.equals(
-              // The cast is necessary here as otherwise you get a stupid error
-              // about a single missing property which is only available
-              // for the 'education guilds'
-              // TODO: though I do want a proper fix for this
-              component.builder.toJSON() as ApplicationCommandData
-            )
-          ) {
-            await commands.get(id)?.edit(fullCommandData);
-          }
-        }
-      } else {
-        await commandManager.create(fullCommandData);
       }
-    }
 
-    this.logger.info("Deployed all application commands");
+      this.logger.info("Deployed all application commands");
+    }
   }
 }
